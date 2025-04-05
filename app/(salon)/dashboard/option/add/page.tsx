9bd180f13039a7b8b9becc6f3ef408ab9a07b2b2
Atsuki Sakai <atk721@icloud.com>
2025-04-06 04:18:47 +0900

    feat: オプション追加ページを追加

--- app/(salon)/dashboard/option/add/page.tsx
+++ app/(salon)/dashboard/option/add/page.tsx
@@ -1,5 +1,14 @@
 import OptionAddForm from './OptionAddForm';
+import { DashboardSection } from '@/components/common';
 
 export default function OptionAddPage() {
-  return <OptionAddForm />;
+  return (
+    <DashboardSection
+      title="オプションを作成"
+      backLink="/dashboard/option"
+      backLinkTitle="オプション一覧"
+    >
+      <OptionAddForm />
+    </DashboardSection>
+  );
 }
