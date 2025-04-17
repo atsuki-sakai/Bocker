ef6e8dbaae3d633c24752fcaadb5d1faaae2bc1b
Atsuki Sakai <atk721@icloud.com>
2025-04-18 08:13:40 +0900

    style: CouponListのレスポンシブパディングを調整

--- app/(salon)/dashboard/coupon/CouponList.tsx
+++ app/(salon)/dashboard/coupon/CouponList.tsx
@@ -47,7 +47,7 @@ export default function CouponList() {
         </p>
       </div>
       <div className="pt-2 flow-root">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 px-4">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 px-4 md:px-0">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <div className="overflow-hidden border border-gray-200 rounded-lg">
               <table className="min-w-full divide-y divide-gray-300">
