69f7f3569cb857f7efc59658175c0e5223394c7f
Atsuki Sakai <atk721@icloud.com>
2025-04-18 01:25:14 +0900

    feat: ダッシュボードページに紹介プログラムカードを追加

--- app/(salon)/dashboard/page.tsx
+++ app/(salon)/dashboard/page.tsx
@@ -1,31 +1,9 @@
-'use client';
-
-import { api } from '@/convex/_generated/api';
-import { useQuery } from 'convex/react';
-import { useSalon } from '@/hooks/useSalon';
+import ReferralCard from '@/components/common/ReferralCard';
 
 export default function DashboardPage() {
-  const { salonId } = useSalon();
-  const referral = useQuery(
-    api.salon.referral.query.findBySalonId,
-    salonId
-      ? {
-          salonId: salonId,
-        }
-      : 'skip'
-  );
   return (
     <div>
-      <h1>Dashboard</h1>
-      <br />
-      <p>招待コード: {referral?.referralCode}</p>
-      {referral?.referralCount && (
-        <p>
-          招待回数: {referral.referralCount}回
-          <br />
-          総招待回数: {referral.totalReferralCount}回
-        </p>
-      )}
+      <ReferralCard />
     </div>
   );
 }
