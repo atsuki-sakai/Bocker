389357b31d509dcfdf141b6e13e4d5365a3e5ee7
Atsuki Sakai <atk721@icloud.com>
2025-04-18 01:25:07 +0900

    refactor: ダッシュボードレイアウトの最適化

--- app/(salon)/dashboard/layout.tsx
+++ app/(salon)/dashboard/layout.tsx
@@ -5,8 +5,8 @@ import { Sidebar } from '@/components/common';
 import { serverConvexAuth } from '@/lib/auth/auth-server';
 import '../../globals.css';
 export const metadata: Metadata = {
-  title: 'Bcker/ブッカー - Dashboard',
-  description: 'Bcker/ブッカーはサロンの予約管理を便利にするサービスです。',
+  title: 'Bcker - ダッシュボード',
+  description: 'Bckerはサロンの予約管理を便利にするサービスです。',
   icons: {
     icon: '/convex.svg',
   },
@@ -17,23 +17,13 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { userId, token, orgId } = await serverConvexAuth();
-  let preloadedSalon;
-  if (!orgId) {
-    preloadedSalon = await preloadQuery(
-      api.salon.core.query.findByClerkId,
-      { clerkId: userId },
-      { token: token }
-    );
-  } else {
-    preloadedSalon = await preloadQuery(
-      api.salon.core.query.findByOrganizationId,
-      { organizationId: orgId },
-      { token: token }
-    );
-  }
+  const { userId, token } = await serverConvexAuth();
 
-  console.log('preloadedSalon: ', preloadedSalon);
+  const preloadedSalon = await preloadQuery(
+    api.salon.core.query.findByClerkId,
+    { clerkId: userId },
+    { token: token }
+  );
 
   return <Sidebar preloadedSalon={preloadedSalon}>{children}</Sidebar>;
 }
