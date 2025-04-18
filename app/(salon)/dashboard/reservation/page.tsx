e8a295957e1c645059c94ea0be2992473fc22e0c
Atsuki Sakai <atk721@icloud.com>
2025-04-19 02:52:11 +0900

    予約管理ページを更新: ReservationListコンポーネントを導入

--- app/(salon)/dashboard/reservation/page.tsx
+++ app/(salon)/dashboard/reservation/page.tsx
@@ -1,4 +1,17 @@
+import ReservationList from './ReservationList';
+
+import { DashboardSection } from '@/components/common';
 
 export default function ReservationPage() {
-  return <div>ReservationPage</div>;
+  return (
+    <DashboardSection
+      title="予約管理"
+      backLink="/dashboard"
+      backLinkTitle="ダッシュボード"
+      infoBtn={{ text: '予約の作成', link: '/dashboard/reservation/add' }}
+    >
+      <p className="text-sm text-gray-500">予約の確認・作成・編集・削除を行うことができます。</p>
+      <ReservationList />
+    </DashboardSection>
+  );
 }
