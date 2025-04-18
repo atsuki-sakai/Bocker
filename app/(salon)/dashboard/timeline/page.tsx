3acce32d3d8c31ae7fc9cc6eb5f9336187c3e1f2
Atsuki Sakai <atk721@icloud.com>
2025-04-18 11:24:06 +0900

    feat: タイムラインページの新規追加

---
+++ app/(salon)/dashboard/timeline/page.tsx
@@ -0,0 +1,13 @@
+import { DashboardSection } from '@/components/common';
+
+export default function TimelinePage() {
+  return (
+    <DashboardSection
+      title="タイムライン"
+      backLink="/dashboard"
+      backLinkTitle="ダッシュボードに戻る"
+    >
+      <div>TimelinePage</div>
+    </DashboardSection>
+  );
+}
