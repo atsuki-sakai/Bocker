1873e91e50b51bd2e955c5248c9c7c1646b01df5
Atsuki Sakai <atk721@icloud.com>
2025-05-07 11:08:50 +0900

    checkbox.tsx: チェック時の色・クラス名をshadcn/ui準拠に修正

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -13,16 +13,18 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'peer h-4 w-4 shrink-0 rounded-sm border border-indigo-300 shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-indigo-600 data-[state=checked]:text-white',
+      "peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
       className
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
+    <CheckboxPrimitive.Indicator
+      className={cn("flex items-center justify-center text-current")}
+    >
       <Check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
-));
+))
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
 export { Checkbox }
