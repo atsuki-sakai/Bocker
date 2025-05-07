3bc6b98c5f391543cada0beb1d12115e01b05ffd
Atsuki Sakai <atk721@icloud.com>
2025-05-07 11:00:02 +0900

    オプションリストのUIカラー・クラス名をshadcn/ui準拠に修正

--- app/(salon)/dashboard/option/OptionList.tsx
+++ app/(salon)/dashboard/option/OptionList.tsx
@@ -62,44 +62,44 @@ export default function OptionList() {
     <div className="mt-2 flow-root">
       <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 px-4 md:px-0">
         <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-          <div className="overflow-hidden border border-gray-200 rounded-lg">
-            <table className="min-w-full divide-y divide-gray-300">
-              <thead className="bg-gray-50 text-nowrap px-2">
+          <div className="overflow-hidden border border-border rounded-lg">
+            <table className="min-w-full divide-y divide-border">
+              <thead className="bg-muted text-nowrap px-2">
                 <tr>
                   <th
                     scope="col"
-                    className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                    className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-primary sm:pl-6"
                   >
                     ステータス
                   </th>
                   <th
                     scope="col"
-                    className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                    className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-primary sm:pl-6"
                   >
                     メニュー名
                   </th>
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-primary"
                   >
                     単価/セール価格
                   </th>
 
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-primary"
                   >
                     最大注文数
                   </th>
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-primary"
                   >
                     実施術時間/トータル施術時間
                   </th>
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-primary"
                   >
                     タグ
                   </th>
@@ -111,34 +111,34 @@ export default function OptionList() {
                   </th>
                 </tr>
               </thead>
-              <tbody className="divide-y divide-gray-200 bg-white text-nowrap">
+              <tbody className="divide-y divide-border bg-background text-nowrap">
                 {options && options.length > 0 ? (
                   options.map((option: Doc<'salon_option'>) => (
                     <tr key={option._id}>
                       <td
-                        className={`py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-6 `}
+                        className={`py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-primary sm:pl-6 `}
                       >
                         <span
-                          className={`font-bold text-xs ${option.isActive ? 'bg-green-600' : 'bg-gray-400'} text-white px-2 py-1 rounded-md`}
+                          className={`font-bold text-xs ${option.isActive ? 'bg-active text-white' : 'bg-destructive text-white'} px-2 py-1 rounded-md`}
                         >
                           {option.isActive ? '有効' : '無効'}
                         </span>
                       </td>
-                      <td className="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-6">
+                      <td className="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-primary sm:pl-6">
                         {option.name}
                       </td>
                       <td className="px-3 py-4 text-sm whitespace-nowrap text-gray-500">
                         {option.salePrice ? (
-                          <span className="line-through text-gray-500 text-xs">
+                          <span className="line-through text-muted-foreground text-xs">
                             ¥{option.unitPrice?.toLocaleString()}
                           </span>
                         ) : (
-                          <span className="text-sm text-gray-700">
+                          <span className="text-sm text-muted-foreground">
                             ¥{option.unitPrice?.toLocaleString()}
                           </span>
                         )}
                         {option.salePrice ? (
-                          <span className="text-sm text-gray-700">
+                          <span className="text-sm text-muted-foreground">
                             / ¥{option.salePrice.toLocaleString()}
                           </span>
                         ) : (
@@ -146,20 +146,20 @@ export default function OptionList() {
                         )}
                       </td>
 
-                      <td className="px-3 py-4 text-sm whitespace-nowrap text-gray-500">
+                      <td className="px-3 py-4 text-sm whitespace-nowrap text-muted-foreground">
                         {option.orderLimit ? `${option.orderLimit}個` : '未設定'}
                       </td>
-                      <td className="px-3 py-4 text-sm whitespace-nowrap text-gray-500">
+                      <td className="px-3 py-4 text-sm whitespace-nowrap text-muted-foreground">
                         {option.timeToMin ? `${option.timeToMin}分` : '未設定'}
                         {option.ensureTimeToMin ? `/ ${option.ensureTimeToMin}分` : ''}
                       </td>
-                      <td className="px-3 py-4 text-sm whitespace-nowrap text-gray-500">
+                      <td className="px-3 py-4 text-sm whitespace-nowrap text-muted-foreground">
                         {option.tags && option.tags.length > 0 ? option.tags.join('、') : '未設定'}
                       </td>
                       <td className="relative py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-6">
                         <Link
                           href={`/dashboard/option/${option._id}/edit`}
-                          className="text-indigo-600 hover:text-indigo-900"
+                          className="text-link hover:text-link-foreground"
                         >
                           <Button variant="ghost" size="sm">
                             編集<span className="sr-only">, {option.name}</span>
@@ -170,7 +170,7 @@ export default function OptionList() {
                         <Button
                           variant="ghost"
                           onClick={() => showDeleteDialog(option._id)}
-                          className="text-red-600 hover:text-red-900"
+                          className="text-destructive hover:text-destructive-foreground"
                         >
                           削除<span className="sr-only">, {option.name}</span>
                         </Button>
@@ -179,7 +179,7 @@ export default function OptionList() {
                   ))
                 ) : (
                   <tr>
-                    <td colSpan={8} className="text-center py-6 text-sm text-gray-500">
+                    <td colSpan={8} className="text-center py-6 text-sm text-muted-foreground">
                       オプションがありません
                     </td>
                   </tr>
