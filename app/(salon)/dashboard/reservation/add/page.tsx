eccf702793050f11ad605893ec5a430be59c4f0a
Atsuki Sakai <atk721@icloud.com>
2025-05-08 03:24:50 +0900

    予約追加ページの説明文・不要なdivとテキストの削除によるシンプル化

--- app/(salon)/dashboard/reservation/add/page.tsx
+++ app/(salon)/dashboard/reservation/add/page.tsx
@@ -4,10 +4,7 @@ import ReservationForm from './ReservationForm';
 export default function ReservationAddPage() {
   return (
     <DashboardSection title="予約の作成" backLink="/dashboard/reservation" backLinkTitle="予約管理">
-      <div className="flex flex-col gap-4">
-        <p className="text-sm text-gray-500">予約の作成を行うことができます。</p>
-      </div>
       <ReservationForm />
     </DashboardSection>
-  );
+  )
 }
