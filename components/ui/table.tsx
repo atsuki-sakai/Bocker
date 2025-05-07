f4b05375f79974df8ed144e27b2a410234e64413
Atsuki Sakai <atk721@icloud.com>
2025-05-07 11:10:07 +0900

    table.tsx: TableHeadの色・クラス名をshadcn/ui準拠に修正

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -73,12 +73,12 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      'h-10 px-2 text-left align-middle font-medium text-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
+      "h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className
     )}
     {...props}
   />
-));
+))
 TableHead.displayName = "TableHead"
 
 const TableCell = React.forwardRef<
