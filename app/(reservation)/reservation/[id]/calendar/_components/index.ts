81072476f6012de99ad75de7459d20aecf0f3a88
Atsuki Sakai <atk721@icloud.com>
2025-06-11 19:32:55 +0900

    chore(i18n): remove obsolete app/(reservation)/reservation/[id]/calendar/_components/index.ts after multilingual refactor (taiyo)

--- app/(reservation)/reservation/[id]/calendar/_components/index.ts
+++
@@ -1,6 +0,0 @@
-export { MenuView } from './MenuView';
-export { OptionView } from './OptionView';
-export { StaffView } from './StaffView';
-export { DateView } from './DateView';
-export { PaymentView } from './PaymentView';
-export { ConfirmView } from './ConfirmView'
