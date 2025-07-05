3c27c564df1096fdeceffe3f9012a84fee6add1c
Atsuki Sakai <atk721@icloud.com>
2025-07-05 22:51:56 +0900

    ダッシュボードにReservationNotificationListコンポーネントを追加

--- app/[locale]/(dashboard)/dashboard/page.tsx
+++ app/[locale]/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import { Tutorial } from '@/components/common/Tutorial'
 import ReservationTimeLine from './reservation/_components/ReservationTimeLine'
-
+import { ReservationNotificationList } from '@/components/common'
 export default async function DashboardPage() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-bold">ダッシュボード</h1>
+      <ReservationNotificationList />
       <ReservationTimeLine />
       <Tutorial />
     </div>
