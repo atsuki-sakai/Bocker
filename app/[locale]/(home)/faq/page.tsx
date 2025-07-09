a7baca93a1c89fcc7c6cf4e3fa812c7d344e350a
Atsuki Sakai <atk721@icloud.com>
2025-07-09 17:49:27 +0900

    FAQページ(app/[locale]/(home)/faq/page.tsx)の変更内容をコミット：内容の詳細はDiff参照

--- app/[locale]/(home)/faq/page.tsx
+++ app/[locale]/(home)/faq/page.tsx
@@ -1,7 +1,15 @@
 import { FAQ } from '../_components/FAQ'
+import { Header } from '../_components/Header'
+import { Footer } from '../_components/Footer'
 import { useLocale } from 'next-intl'
 
 export default function FAQPage() {
   const locale = useLocale()
-  return <FAQ locale={locale} />
+  return (
+    <>
+      <Header />
+      <FAQ locale={locale} />
+      <Footer />
+    </>
+  )
 }
